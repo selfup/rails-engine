156ba97ede6d3d1df43e23d87e7fe4bd917db828
selfup <boudinot.regis@yahoo.com>
2015-11-03 13:59:33 -0700

    fixed transaction test

--- test/controllers/api/v1/transactions_controller_test.rb
+++ test/controllers/api/v1/transactions_controller_test.rb
@@ -8,7 +8,7 @@ class Api::V1::TransactionsControllerTest < ActionController::TestCase
   end
 
   test '#index returns the right number of transactions' do
-    number_of_Transactions = Transaction.count
+    number_of_transactions = Transaction.count
 
     get :index, format: :json
 
@@ -34,6 +34,6 @@ class Api::V1::TransactionsControllerTest < ActionController::TestCase
     assert_equal item.invoice_id, json_response["invoice_id"]
     assert_equal item.credit_card_number, json_response["credit_card_number"]
     assert_equal item.credit_card_expiration_date, json_response["credit_card_expiration_date"]
-    assert_equal item.result, json_response["result"]    
+    assert_equal item.result, json_response["result"]
   end
 end
