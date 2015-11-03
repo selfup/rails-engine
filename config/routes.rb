b685055c0e662b18d119500d88472dae883d8b78
selfup <boudinot.regis@yahoo.com>
2015-11-02 18:07:21 -0700

    cleaned up gemfile - added working endpoints - json displays in the views - going to work on custom routes soon

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  
+  namespace :api do
+    namespace :v1 do
+      resources :customers, defaults: { format: 'json' }
+      resources :transactions, defaults: { format: 'json' }
+      resources :items, defaults: { format: 'json' }
+      resources :merchants, defaults: { format: 'json' }
+      resources :invoices, defaults: { format: 'json' }
+      resources :invoice_items, defaults: { format: 'json' }
+    end
+  end
 end
