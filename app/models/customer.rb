491324b7c0b34a2f3980d80cf2c8f65e05cdefba
selfup <boudinot.regis@yahoo.com>
2015-11-03 16:29:00 -0700

    got favorite merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,8 +8,15 @@ class Customer < ActiveRecord::Base
   end
 
   def favorite_merchant
+    binding.pry
     fav_id = invoices.successful.group(:merchant_id).count.first[0]
     Merchant.find_by(id: fav_id)
+
+    # Merchant.select("invoices.*, count(order_items.customer_id) AS items_count").
+    #                       joins(:order_items).
+    #                       group("items.id").
+    #                       order("items_count DESC").
+    #                       take(2)
   end
 
 end
