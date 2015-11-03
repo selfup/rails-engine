e214e9b47a1690d51f60be9d74d6928be0002875
selfup <boudinot.regis@yahoo.com>
2015-11-02 20:52:58 -0700

    got invoices to show by customer_id

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -21,6 +21,14 @@ class Api::V1::CustomersController < ApplicationController
    respond_with Customer.where(customer_params)
   end
 
+  def invoices
+    respond_with Invoice.joins(:customer).where(customer_id: params[:id])
+  end
+
+  def transactions
+
+  end
+
   private
 
   def customer_params
