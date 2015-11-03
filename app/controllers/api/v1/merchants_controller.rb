9b7788696f09a72f415845c10a0f3db82614aba1
selfup <boudinot.regis@yahoo.com>
2015-11-02 18:34:35 -0700

    starting to find by id and name

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,4 +8,14 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     respond_with Merchant.find_by(id: params[:id])
   end
+
+  def find
+    respond_with Merchant.find_by(merchant_params)
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
 end
