a3c10f38d9f9ef3f23c5ff99d6c77e75bbc5c66e
selfup <boudinot.regis@yahoo.com>
2015-11-02 19:21:15 -0700

    Added find and find_all for the rest of the controllers

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -8,4 +8,18 @@ class Api::V1::TransactionsController < ApplicationController
   def show
     respond_with Transaction.find_by(id: params[:id])
   end
+
+  def find
+    respond_with Transaction.find_by(transaction_params)
+  end
+
+  def find_all
+   respond_with Transaction.where(transaction_params)
+  end
+
+  private
+
+  def transaction_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
 end
