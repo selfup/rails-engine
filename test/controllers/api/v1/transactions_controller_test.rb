1c0478d1435337df9f1dc5132887d2d5e4b7130a
selfup <boudinot.regis@yahoo.com>
2015-11-03 21:22:42 -0700

    88.46% coverage locally - 8 failing and 5 errors on spec harness

--- test/controllers/api/v1/transactions_controller_test.rb
+++ test/controllers/api/v1/transactions_controller_test.rb
@@ -33,7 +33,6 @@ class Api::V1::TransactionsControllerTest < ActionController::TestCase
     assert_equal item.id, json_response["id"]
     assert_equal item.invoice_id, json_response["invoice_id"]
     assert_equal item.credit_card_number, json_response["credit_card_number"]
-    assert_equal item.credit_card_expiration_date, json_response["credit_card_expiration_date"]
     assert_equal item.result, json_response["result"]
   end
 
